93ef3f1328ebbb4b5a99db26cc0f2ad647539948
Vadim Dorogin <vadimdorogin65@gmail.com>
2026-02-28 22:03:08 +0300

    f1

--- include/alg.h
+++ include/alg.h
@@ -3,9 +3,19 @@
 #define INCLUDE_ALG_H_
 #include <cstdint>
 
-bool checkPrime(uint64_t value);
-uint64_t nPrime(uint64_t n);
-uint64_t nextPrime(uint64_t value);
-uint64_t sumPrime(uint64_t hbound);
+#ifdef _WIN32
+#ifdef ST1_EXPORTS
+#define ST1_API __declspec(dllexport)
+#else
+#define ST1_API __declspec(dllimport)
+#endif
+#else
+#define ST1_API
+#endif
+
+bool ST1_API checkPrime(uint64_t value);
+uint64_t ST1_API nPrime(uint64_t n);
+uint64_t ST1_API nextPrime(uint64_t value);
+uint64_t ST1_API sumPrime(uint64_t hbound);
 
 #endif  // INCLUDE_ALG_H_
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,17 +1,59 @@
-// Copyright 2025 UNN-CS
-
 #include <gtest/gtest.h>
-#include <cstdint>
 #include "alg.h"
 
 
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+TEST(CheckPrimeTest, HandlesSmallPrimes) {
+    EXPECT_TRUE(checkPrime(2));
+    EXPECT_TRUE(checkPrime(3));
+    EXPECT_TRUE(checkPrime(5));
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+
+TEST(CheckPrimeTest, HandlesNonPrimes) {
+    EXPECT_FALSE(checkPrime(0));
+    EXPECT_FALSE(checkPrime(1));
+    EXPECT_FALSE(checkPrime(4));
+    EXPECT_FALSE(checkPrime(100));
+}
+
+TEST(CheckPrimeTest, LargePrime) {
+    EXPECT_TRUE(checkPrime(97));
+}
+
+
+TEST(NPrimeTest, FirstPrime) {
+    EXPECT_EQ(nPrime(1), 2);
+}
+
+TEST(NPrimeTest, SeveralPrimes) {
+    EXPECT_EQ(nPrime(2), 3);
+    EXPECT_EQ(nPrime(3), 5);
+    EXPECT_EQ(nPrime(5), 11);
+}
+
+
+TEST(NextPrimeTest, BasicCases) {
+    EXPECT_EQ(nextPrime(4), 5);
+    EXPECT_EQ(nextPrime(11), 13);
+}
+
+TEST(NextPrimeTest, FromPrime) {
+    EXPECT_EQ(nextPrime(2), 3);
+}
+
+
+TEST(SumPrimeTest, SmallBound) {
+    EXPECT_EQ(sumPrime(10), 17); 
+}
+
+TEST(SumPrimeTest, ZeroAndTwo) {
+    EXPECT_EQ(sumPrime(0), 0);
+    EXPECT_EQ(sumPrime(2), 0);
+}
+
+TEST(SumPrimeTest, LargerBound) {
+    EXPECT_EQ(sumPrime(20), 77); 
+}
+
+TEST(DummyTest, Smoke) {
+    EXPECT_TRUE(true);
 }
